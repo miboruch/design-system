659290bd41347e3af1b0c2496efe00038571ba01
Michał Boruch <miboruch@gmail.com>
2022-11-01 12:12:25 +0100

    feat: eslint config

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,14 +3,18 @@ import React from "react";
 import styles from "./button.module.scss";
 
 export type ButtonProps = {
-  text: string;
+  children: React.ReactNode;
   variant: "primary" | "secondary";
 } & React.ButtonHTMLAttributes<any>;
 
-export const Button: React.FC<ButtonProps> = ({ type, text, variant }) => {
+export const Button: React.FC<ButtonProps> = ({ type, variant, children, ...rest }) => {
   return (
-    <button type={type} className={variant === "primary" ? styles.primary : styles.secondary}>
-      {text}
+    <button
+      type={type}
+      className={variant === "primary" ? styles.primary : styles.secondary}
+      {...rest}
+    >
+      {children}
     </button>
   );
 };
